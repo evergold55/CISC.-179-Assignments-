d59d8880848ea66dcb7a3c54f4a0c0c66db0ff0b
evergold55 <everald55@gmail.com>
2025-09-07 20:10:56 -0700

    Delete First code.Golding directory

--- First code.Golding/First Code.Golding.py
+++
@@ -1,3 +0,0 @@
-print("All the world's a stage,\nAnd all the men and women merely players:\nThey have their exits and their entrances;\nAnd one man in his time plays many parts,\nHis acts being seven ages.")
-
-
