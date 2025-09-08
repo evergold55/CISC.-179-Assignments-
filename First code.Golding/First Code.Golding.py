1c6301a02200a5b812c774d32c299c8f30b26111
evergold55 <everald55@gmail.com>
2025-09-07 20:01:28 -0700

    Add files via upload

---
+++ First code.Golding/First Code.Golding.py
@@ -0,0 +1,3 @@
+print("All the world's a stage,\nAnd all the men and women merely players:\nThey have their exits and their entrances;\nAnd one man in his time plays many parts,\nHis acts being seven ages.")
+
+
